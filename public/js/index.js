cfa370363d3b128058e4cd159d7c177510df7aa7
Hong Shick Pak <hong@hspak.com>
2014-11-26 18:32:21 -0500

    index responds to bad input

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,32 @@
+redifyHold = false;
+
+function redify(input) {
+  redifyHold = true;
+  input.classList.add('redify');
+  setTimeout(function() {
+    input.classList.remove('redify');
+    input.classList.add('unredify');
+    setTimeout(function() {
+      input.classList.remove('unredify');
+      redifyHold = false;
+    }, 500);
+  }, 500);
+}
+
 function newTodo(event) {
-  input = document.getElementById("searchBox").value;
-  if (event.keyCode == 13 && input.length > 0) {
-    if (! /^[a-zA-Z0-9~!@$\^&\*\(\)\{\}\[\]\+\-\=\_\,\<\>\"\'\:\;\`\|]+$/.test(input)) {
-      helperLabel.innerHTML = "No"
+  input = document.getElementById("searchBox");
+  if (event.keyCode == 13 && input.value.length > 0) {
+    if (! /^[a-zA-Z0-9~!@$\^&\*\(\)\{\}\[\]\+\-\=\_\,\<\>\"\'\:\;\`\|]+$/.test(input.value)) {
+      if (redifyHold == false) {
+        redify(input);
+      }
       return;
-    } else if (input.length > 80) {
-      helperLabel.innerHTML = "80 char limit";
+    } else if (input.value.length > 80) {
+      if (redifyHold == false) {
+        redify(input);
+      }
       return;
-    } 
-    window.location = "http://" + window.location.host + "/list/" + input;
+    }
+    window.location = "http://" + window.location.host + "/list/" + input.value;
   }
 }
